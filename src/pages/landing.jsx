d06ec01f4f2a446f6f472ce22f2ce9636965dafc
BEKKAR MERWAN <bkar.mrwan16@gmail.com>
2023-01-16 09:58:12 +0100

    change picture

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,27 +1,24 @@
 import React, { Fragment } from "react";
-import Navbar from '../components/sharedComponents/NavBar'
+import Navbar from "../components/sharedComponents/NavBar";
 import Main from "../components/Main";
 import About from "../components/About";
 import Query from "../components/Query";
 import Footer from "../components/sharedComponents/Footer";
 import Services from "../components/Services";
 
-
 class Landing extends React.Component {
-    render() {
-        return (
-            <Fragment>
-                <Navbar />
-                <Main />
-                <About />
-                <Services/>
-                <Query/>
-                <Footer/>
-            </Fragment>
-            
-          );
-    }
+  render() {
+    return (
+      <Fragment>
+        <Navbar />
+        <Main />
+        <About />
+        <Services />
+        <Query />
+        <Footer />
+      </Fragment>
+    );
+  }
 }
-
 
 export default Landing;
